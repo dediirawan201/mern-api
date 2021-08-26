3adce37ae3ac54be669e6f4e9ccae50d17246516
dedi <dediirwansyah1925@gmail.com>
2021-08-26 09:35:08 +0700

    memvalidasi request body dan memberikan error response dinamis

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,8 +1,23 @@
+import { validationResult } from "express-validator";
+
 export const createBlogPost = (req,res,next) => {
     const title = req.body.title;
     const image = req.body.image;
     const body = req.body.body;
 
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        const err = new Error('input invalid error');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err; 
+        // console.log('err :', errors)
+        // res.status(400).json({
+        //     message: "Request Error",
+        //     data:null,
+        // })
+    }
     const result = {
         message: "Create Blog Post Success",
         data: {
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { createBlogPost } from '../controllers/blog.js';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
 // [POST] : /v1/blog/post 
-router.post('/post',createBlogPost)
+router.post('/post',[
+    body('title').isLength({min:5}).withMessage('input title tidak sesuai'),
+    body('body').isLength({min:5}).withMessage('input body tidak sesuai')]
+    ,createBlogPost)
 
 export default router;
